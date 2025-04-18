e07ea1bec43f26954bcf51bef70cf596a5c946a4
Arin7712 <aringawande7712@gmail.com>
2025-04-18 23:40:11 +0530

    kk

--- components/Awards.tsx
+++ components/Awards.tsx
@@ -8,16 +8,23 @@ const Awards = () => {
       <div className="w-full overflow-x-auto">
         <table className="w-full table-auto">
           <tbody>
-            <tr className="flex justify-center md:justify-between md:gap-0 gap-[4rem] md:px-[6rem] flex-wrap">
+            <tr className="flex justify-center md:justify-between md:gap-0 gap-[8rem] md:px-[6rem] flex-wrap">
               {awardItems.map((item, index) => (
-                <td key={index} className="w-[200px] flex flex-col items-center group cursor-pointer">
-                  <div className="w-[200px] h-[200px] flex items-center justify-center overflow-hidden">
+                <td key={index} className="md:w-[200px] w-[130px] flex flex-col items-center group cursor-pointer">
+                  <div className="md:w-[200px] md:h-[200px] w-[130px] h-[130px] flex items-center justify-center overflow-hidden">
                     <Image
                       src={item.img}
                       alt="logo"
                       width={200}
                       height={200}
-                      className="object-contain transition-all duration-300 group-hover:scale-110"
+                      className="object-contain transition-all duration-300 group-hover:scale-110 md:block hidden"
+                    />
+                    <Image
+                      src={item.img}
+                      alt="logo"
+                      width={130}
+                      height={130}
+                      className="object-contain transition-all duration-300 group-hover:scale-110 md:hidden block"
                     />
                   </div>
                   <p className="uppercase text-xs font-medium underline-offset-2 transition-all group-hover:underline text-center mt-2">
