fc18efc3d7a35646d66fbbfbf8978475805c9000
Arin7712 <aringawande7712@gmail.com>
2025-04-19 15:14:02 +0530

    effect

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-[#f1f1f1]">
-      <Hero/>
-      <Awards/>
-      <Work/>
-    </div>
+<div className="relative bg-[#f1f1f1] overflow-hidden">
+  {/* Stronger Background Grain Effect */}
+  <div className="pointer-events-none fixed top-0 left-0 w-full h-full opacity-30 z-0 grainy-background"></div>
+
+  {/* Main Content */}
+  <div className="relative z-10">
+    <Hero />
+    <Awards />
+    <Work />
+  </div>
+</div>
   );
 }
